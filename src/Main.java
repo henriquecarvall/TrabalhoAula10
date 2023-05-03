30ff8359022310a10ba13d1d09193c78ed6d6c1c
Henrique Carvall <carvallhen@gmail.com>
2023-05-03 20:22:16 -0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
 public class Main {
-    System.out.println ("Hello World")
+    public static void main(String[] args) {
+        String[] Animais = {"Tartaruga, Aguia, Leão"};
+        for (String i : Animais) {
+            System.out.println(i + " são animais fortes");
+        }
+
+        Arrays.sort(Animais);
+        System.out.println("Agora invertendo a ordem");
+        for (String i : Animais) {
+            System.out.println(i + " São animais fracos");
+
+        }
+    }
+
 }
